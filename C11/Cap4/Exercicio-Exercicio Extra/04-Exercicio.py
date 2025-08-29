f7a070c5b0b8fd4d8546c0ec161b78a08fe18183
GalaxyIgor <igornbelisario@gmail.com>
2025-08-28 21:08:49 -0300

    Update 04-Exercicio.py

--- C11/Cap4/Exercicio-Exercicio Extra/04-Exercicio.py
+++ C11/Cap4/Exercicio-Exercicio Extra/04-Exercicio.py
@@ -7,8 +7,11 @@ ds = np.loadtxt('paises.csv',
                 dtype=str,
                 encoding='utf-8')
 
-# Cria máscara para "NORTHERN AMERICA"
-NAMask = np.sum(np.char.find(ds,'NORTHERN AMERICA')!= -1)
-
-
-print(f"Quantidade de países na América do Norte: {NAMask}")
+rendas = ds[0:,9]
+region = ds[0:,1]
+america_index = np.where(region == 'LATIN AMER. & CARIB    ')
+rendas_america = rendas[america_index]
+local = ds[0:,0]
+contry = local[america_index]
+maiorRenda = np.argmax(rendas_america)
+print(contry[maiorRenda])do Norte: {NAMask}")
